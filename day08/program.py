23f9b915c50c743410ee3204ee78671ec3cc9663
Carlos <carlosmgov7k@gmail.com>
2023-06-17 12:26:11 +0200

    day 08

--- day08/program.py
+++ day08/program.py
@@ -3,11 +3,11 @@ import re
 file = open("input.txt")
 code = 0
 lit = 0
+pat = 0
+pattern = r"[\\\"]"
 for line in file:
-    code = len(line.strip())
-    patternHex = r"\\x\w\w"
-    patternSla = r"\\\W"
-    m = len(re.findall(patternHex, line))
-    n = len(re.findall(patternSla, line))
-    lit = (len(line.strip()) - (2 + (m*4) + (n*2))) + (m+n)
-print(code - lit)
+    code += len(line.strip())
+    lit += len(eval(line))
+    pat += len(line.strip()) + 2 + len(re.findall(pattern, line))
+print(code-lit)
+print(pat-code)
