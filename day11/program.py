bbcefd5b139f3942b918b7b8c509e49633689bfb
Carlos <carlosmgov7k@gmail.com>
2023-08-01 10:19:43 +0200

    no IA will take my place fk computers. Day 11.

--- day11/program.py
+++ day11/program.py
@@ -1,44 +1,17 @@
-import re
+# Solved by reasoning
+# clearly the password must match two pair of letters and a three letter consecutive chain.
+# since closest password will be editing as few digits to the right as possible,
+# we are looking for a pattern in which we see an XXYZZ pattern
+# therefore, from 'hxbxwxba' input, closest is hxbx (fixed) then the same last letter, x, then the pattern
+# from x, yzz.
+#
+# First result hxbxxyzz.
+#
+# The second comes to the same thing, we gotta increase the letters, then coil them around the rules.
+# increasing z we cant get our pattern right so closest will be cc, therefore, previous letter will be b,
+# and previous to this, two a. Since we cant increase the letter up to a, we increase
+# the fifth letter from the last position.
+#
+# Second result hxcaabcc.
 
 
-def cont(input):
-    for i in range(len(input) - 2):
-        if input[i] + input[i + 1] + input[i + 2] in 'abcdefghijklmnopqrstuvwxyz':
-            return True
-    return False
-
-
-def iol(input):
-    if any(('i', 'o', 'l') for c in input):
-        return False
-    return True
-
-
-def reg(input):
-    m = re.search('.*([a-z])\1.*([a-z])\2.*', input)
-    if m == None:
-        return False
-    return True
-
-
-og = 'hxbxwxba'
-input = og
-initial_state = int(len(input)/2)
-first_half = input[:initial_state]
-second_half = input[initial_state:]
-
-ok = False
-second_half = chr(ord(first_half[len(first_half) - 1])) + second_half[1:]
-while (not ok):
-    last = len(second_half) - 1
-    while (second_half[last] == 'z'):
-        second_half = second_half[:last] + 'a' + second_half[last + 1:]
-        last -= 1
-    second_half = second_half[:last] + chr(ord(second_half[last]) + 1) + second_half[last + 1:]
-    print(first_half + second_half)
-    if(len(first_half + second_half) != 8):
-        input = og
-        first_half = input[:initial_state-1]
-        second_half = input[initial_state+1:]
-    ok = cont(first_half + second_half) and reg(first_half + second_half) and iol(first_half + second_half)
-print('Result: ' + first_half + second_half)
